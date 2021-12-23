b68959f5c05986591d6245103f05f9b0f91c9f77
Clever Coder <clevercoderr@gmail.com>
2021-12-23 10:51:07 +0500

    Add project files.

---
+++ Zamin Education Page Backend Course Video Playlists/OutputPageNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zamin_Education_Page_Backend_Course_Video_Playlists
+{
+    internal class OutputPageNumber
+    {
+        //var Objects = new List<string>();
+
+    }
+}
---
+++ Zamin Education Page Backend Course Video Playlists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Zamin_Education_Page_Backend_Course_Video_Playlists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int pageCount = int.Parse(Console.ReadLine());
+            int pageIndex = int.Parse(Console.ReadLine());
+
+            //for (int i = pageIndex * pageCount - pageCount; i < pageIndex * pageCount ; i++ )
+            //{
+            //    Console.Write(nums[i] + " ");
+            //}
+
+            var items = nums.Skip(pageCount*pageIndex - pageCount).Take(pageCount);
+
+            foreach (var item in items)
+            {
+                Console.Write(item + " ");
+            }
+
+
+        }
+    }
+}
